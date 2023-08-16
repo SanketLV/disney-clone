440971effbc04ab345c08c32c3574c1147e5cb91
SanketLakhani <sanketlakhani80@gmail.com>
2023-08-16 10:59:54 +0530

    Details page added and deployed in firebase.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Detail from "./components/Detail";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Login from "./components/Login";
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Login />}></Route>
           <Route exact path="/home" element={<Home />}></Route>
+          <Route exact path="/detail/:id" element={<Detail />}></Route>
         </Routes>
       </Router>
     </div>
